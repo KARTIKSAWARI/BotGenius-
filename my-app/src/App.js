f94b8e56b18cecc4bff52e89416201e622dc50c0
Vikas1999-TS <vikasts9964125687@gmail.com>
2023-04-20 10:54:35 +0530

    Features css file

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Features.css'
+import Features from './Features';
+
+
+
+
+function App() {
+  return (
+   
+    <div className='App'>
+      <Features/>
+     
+     
+     </div>
+      
+  
+  );
+}
+
+export default App;
---
+++ my-app/src/Features.js
@@ -0,0 +1,138 @@
+import React from 'react';
+
+import './Features.css'
+function Features() {
+  return (
+    <div className='maindiv' style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', maxWidth: '100vw' }}>
+      <div className='img' style={{ display: 'flex', flexDirection: 'row', padding: '0px', maxWidth: '100%', boxSizing: 'border-box' }} >
+        <div className='FeaturesHeading'>
+
+          <h1>Features That Drive Your Business</h1>
+          <p style={{ maxWidth: '600px', margin: '0 auto', textAlign: 'justify' }}>
+            Discover the ultimate suite of AI-powered tools designed to revolutionize
+            your professional workflow.ChartGenius+, ChartGenius, Doc Genius, Test Genius,
+            and PM Genius offer innovative solutions for visual thinking, document management,
+            testing processes, and project management, propelling your business to new heights
+            of productivity and success. Embrace the future and let these cutting-edge tools
+            transform the way you work.
+          </p>
+        </div>
+        <div className='img1'>
+        </div>
+      </div>
+
+
+
+      <div style={{ maxWidth: '1250px', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        <div style={{ padding: '30px' }} >
+          <h2>1.ChartGenius+ :</h2>
+          <p style={{ textAlign: 'justify', marginBottom: '10px' }}>
+            Experience a next-generation AI assistant that provides users with sophisticated, detailed answers to their questions, complemented by
+            visually engaging diagrams and flowcharts. ChartGenius+ offers a comprehensive and visually striking approach to problem-solving.
+          </p>
+        </div>
+        <div className='hexabackground' style={{ flex: '0 0 50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ width: '80%', height: '70%', background: '#E5E7EC', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)', border: '10px solid #fff', marginRight: '100px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="play-button" width="106" height="106" fill="#52C6D9" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+
+          </div>
+        </div>
+      </div>
+
+
+      <div style={{ maxWidth: '1250px', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        
+        <div className='hexabackground2' style={{ flex: '0 0 50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ width: '80%', height: '70%', background: '#E5E7EC', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)', border: '10px solid #fff', marginLeft: '100px',marginTop:'50px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="play-button" width="106" height="106" fill="#52C6D9" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+
+          </div>
+        </div>
+        <div style={{ padding: '30px' }} >
+          <h2>2.ChartGenius :</h2>
+          <p style={{ textAlign: 'justify', marginBottom: '10px' }}>
+          Unlock the power of visual thinking with this versatile tool,offering three
+            innovative features for creating diagrams, flowcharts, and mind maps.  ChartGenius enables
+            users to design visually compelling and well-organized charts for a wide range of applications.
+          </p>
+        </div>
+      </div>
+
+
+      <div style={{ maxWidth: '1250px', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        <div style={{ padding: '30px' }} >
+          <h2>3.Doc Genius:</h2>
+          <p style={{ textAlign: 'justify', marginBottom: '10px' }}>
+          Transform the way you work with PDFs through three essential features : PDF
+            Summarization for distilling complex documents, PDF Question Generator for proactive content
+            analysis,  and PDF Question answers for interactive and accurate content inquiries.  Doc Genius is
+            the ultimate tool for efficient document management.
+          </p>
+        </div>
+        <div className='hexabackground' style={{ flex: '0 0 50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ width: '80%', height: '70%', background: '#E5E7EC', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)', border: '10px solid #fff', marginRight: '100px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="play-button" width="106" height="106" fill="#52C6D9" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+
+          </div>
+        </div>
+      </div>
+
+      <div style={{ maxWidth: '1250px', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        
+        <div className='hexabackground2' style={{ flex: '0 0 50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ width: '80%', height: '70%', background: '#E5E7EC', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)', border: '10px solid #fff', marginLeft: '100px',marginTop:'50px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="play-button" width="106" height="106" fill="#52C6D9" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+
+          </div>
+        </div>
+        <div style={{ padding: '30px' }} >
+          <h2>4.Test Genius:</h2>
+          <p style={{ textAlign: 'justify', marginBottom: '10px' }}>
+          Optimize your testing processes with a comprehensive testing toolkit that includes
+            Test Plan,Test Scenarios, Test Data, User stories, Manual Test cases, and Automation code
+            generation. Test Genius offers an all-in-one solution to streamline both manual and automated
+            testing workflows.
+          </p>
+        </div>
+      </div>
+
+      <div style={{ maxWidth: '1250px', display: 'flex', alignItems: 'center', flexDirection: 'row' }}>
+        <div style={{ padding: '30px' }} >
+          <h2>5.PM Genius :</h2>
+          <p style={{ textAlign: 'justify', marginBottom: '10px' }}>
+          Elevate your project management skills with a suite of six indispensable features,
+            including Project Charter, SRS Documentation, Deployment Documentation,User Stories,Tasks
+            generation, and Scrum master. PM Genius provides the resources and insights necessary to
+            ensure project success,  from inception to completion.
+          </p>
+        </div>
+        <div className='hexabackground' style={{ flex: '0 0 50%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <div style={{ width: '80%', height: '70%', background: '#E5E7EC', boxShadow: '0 0 5px rgba(0, 0, 0, 0.1)', border: '10px solid #fff', marginRight: '100px' }}>
+            <svg xmlns="http://www.w3.org/2000/svg" class="play-button" width="106" height="106" fill="#52C6D9" viewBox="0 0 16 16">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+
+          </div>
+        </div>
+      </div>
+
+
+    </div>
+
+  );
+}
+
+export default Features;
